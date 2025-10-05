23a92a2f35624fd235d0b89df1be4aec24d83a79
Rohit Bharat Bhandwalkar <rohitb.bhandwalkar@gmail.com>
2025-10-06 01:51:07 +0530

    Added API endpoints to delete,update and fetch employee data

--- verto_backend/controller.js
+++ verto_backend/controller.js
@@ -7,6 +7,7 @@ export const addEmployee = async (req, res, next) => {
 
         if(!errors.isEmpty()){
             return res.status(400).json({
+                success : false,
                 message : "Validation errors",
                 error : errors.array()
             })
@@ -19,8 +20,86 @@ export const addEmployee = async (req, res, next) => {
         });
 
         return res.status(201).json({
+            success : true,
             message : "Employee added",
             employee : employee
+        });
+
+    } catch (err) {
+        next(err);
+    }
+}
+
+export const fetchEmployees = async (req, res, next) => {
+    try{
+        
+        const employees = await prisma.employee.findMany({
+            orderBy : {id : 'asc'}
+        });
+
+        return res.status(200).json({
+            success : true,
+            message : "Employees data fetched",
+            employees : employees
+        });
+    } catch (err) {
+        next(err);
+    }
+}
+
+export const deleteEmployee = async (req, res, next) => {
+    try{
+        const errors = validationResult(req);
+
+        if(!errors.isEmpty()){
+            return res.status(400).json({
+                success : false,
+                message : "Validation errors",
+                errors : errors.array()
+            });
+        }
+        const { employeeId } = req.params;
+
+        const deletedEmployee = await prisma.employee.delete({ 
+            where : { id : parseInt(employeeId)}
+         });
+
+        return res.status(200).json({
+            success : true,
+            message : "Employee data deleted",
+            employee : deletedEmployee
+        });
+
+    } catch (err) {
+        next(err);
+    }
+}
+
+export const updateEmployeeData = async (req, res, next) => {
+    try{
+
+        const errors = validationResult(req);
+
+        if(!errors.isEmpty()){
+            return res.status(400).json({
+                success : false,
+                message : "Validation errors",
+                errors : errors.array()
+            });
+        }
+
+        const { employeeId } = req.params;
+        const updateData = req.body;
+
+        const updatedEmployeeData = await prisma.employee.update({
+            where : { id : parseInt(employeeId) },
+            data : updateData
+        });
+
+        return res.status(200).json({
+            success : true,
+            message : "Employee data updated successfully",
+            employee : updatedEmployeeData
         });
 
     } catch (err) {
--- verto_backend/routes.js
+++ verto_backend/routes.js
@@ -1,19 +1,62 @@
 import { Router } from "express";
-import { addEmployee } from "./controller.js";
-import { body } from "express-validator";
+import {
+  addEmployee,
+  deleteEmployee,
+  fetchEmployees,
+  updateEmployeeData,
+} from "./controller.js";
+import { body, param } from "express-validator";
 import { Departments, Roles } from "./constants/enums";
 
 const routes = Router();
 
-routes.post('/add-employee',
-    [
-        body('name').trim().isLength({min : 3}).withMessage('Name should be atleast 3 characters long'),
-        body('email').trim().isEmail().withMessage('Email address is invalid'),
-        body('phone').trim().matches(/^[0-9]{10}$/).withMessage('Phone number must be atleast 10 characters long'),
-        body('department').trim().isIn(Departments).withMessage('Invalid department'),
-        body('role').trim().isIn(Roles).withMessage('Invalid role'),
-        body('dateOfJoinig').optional().isISO8601().withMessage('Invalid date')
-    ],
-addEmployee);
+routes.post(
+  "/add-employee",
+  [
+    body("name")
+      .trim()
+      .isLength({ min: 3 })
+      .withMessage("Name should be atleast 3 characters long"),
+    body("email").trim().isEmail().withMessage("Email address is invalid"),
+    body("phone")
+      .trim()
+      .matches(/^[0-9]{10}$/)
+      .withMessage("Phone number must be atleast 10 characters long"),
+    body("department")
+      .trim()
+      .isIn(Departments)
+      .withMessage("Invalid department"),
+    body("role").trim().isIn(Roles).withMessage("Invalid role"),
+    body("dateOfJoinig").optional().isISO8601().withMessage("Invalid date"),
+  ],
+  addEmployee
+);
+
+routes.get("/fetch-employees", fetchEmployees);
+
+routes.delete("/delete-employee/:employeeId", param('employeeId').isInt({gt:0}).withMessage("employeeId must be a valid positive integer").toInt(), deleteEmployee);
+
+routes.put(
+  "/update-employee-data/:employeeId",
+  [
+    param('employeeId').isInt({gt:0}).withMessage("employeeId must be a valid positive integer").toInt(),
+    body("name").optional()
+      .trim()
+      .isLength({ min: 3 })
+      .withMessage("Name should be atleast 3 characters long"),
+    body("email").optional().trim().isEmail().withMessage("Email address is invalid"),
+    body("phone").optional()
+      .trim()
+      .matches(/^[0-9]{10}$/)
+      .withMessage("Phone number must be atleast 10 characters long"),
+    body("department").optional()
+      .trim()
+      .isIn(Departments)
+      .withMessage("Invalid department"),
+    body("role").optional().trim().isIn(Roles).withMessage("Invalid role"),
+    body("dateOfJoinig").optional().isISO8601().withMessage("Invalid date"),
+  ],
+  updateEmployeeData
+);
 
 export default routes;
